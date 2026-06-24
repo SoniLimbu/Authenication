2f0ee00a30c55cd3b3aa5382d5eba6ea47aeaeef
acharyanisha1234 <acharyanisha2063gmail.com>
2026-06-24 11:14:24 +0545

    Create authentication page with login and register forms

---
+++ LOGIN-FRONTEND/src/App.jsx
@@ -0,0 +1,14 @@
+import { Routes, Route } from "react-router-dom";
+import "./index.css";
+import AuthPage from "./pages/Authpage";
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<AuthPage />} />
+      
+    </Routes>
+  );
+}
+
+export default App;
